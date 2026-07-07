2814ac3ec3ca4c59a0c36ce2206f22035b2e4704
Chad Lowe <pfmsoft.dev@gmail.com>
2026-07-07 14:52:35 -0700

    drop example code

--- src/api_request/example/cache/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Package for caching HTTP responses."""
--- src/api_request/example/helpers/settings_factories.py
+++
@@ -1,8 +0,0 @@
-"""Factory functions for creating instances of settings-related classes."""
-
-from esi_link.settings import EsiLinkSettings
-
-
-def app_data_db_uri_factory(settings: EsiLinkSettings) -> str:
-    """Factory function to create the URI for the app-data SQLite database."""
-    return f"file:{settings.app_data_db_path}?mode=rwc"
